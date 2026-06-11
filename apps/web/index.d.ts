73d0bf3e08d0cb33eef27d32f79fc6b17cf2cf8d
clnnn <65890025+clnnn@users.noreply.github.com>
2026-06-11 04:21:35 +0000

    fix(types): update module declarations and fix import paths in TypeScript configuration

--- apps/web/index.d.ts
+++ apps/web/index.d.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 declare module '*.svg' {
-  const content: any;
   export const ReactComponent: any;
-  export default content;
 }
 
+declare module '*.css' {
+  const styles: { [key: string]: string };
+  export default styles;
+}
 
